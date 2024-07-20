1b7bbd4ccf4de1a70c482670a6366f0afb425fde
Jonathan <jlslew@users.noreply.github.com>
2024-07-20 08:15:06 +0400

    Update login page to use asset-mapper

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,9 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
