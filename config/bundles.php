5734dc51be7f30d9b2ce916980012da58291b476
Jonathan <jlslew@users.noreply.github.com>
2024-07-20 14:06:28 +0400

    Add js-routing library

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
