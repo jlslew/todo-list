49073c37cdc80cd0a0d8a4c13db3be56c08eda74
Jonathan <jlslew@users.noreply.github.com>
2024-07-20 13:58:24 +0400

    Add migration for message table

---
+++ migrations/Version20240720095753.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240720095753 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, content VARCHAR(255) NOT NULL, task_id INT NOT NULL, INDEX IDX_B6BD307F8DB60186 (task_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F8DB60186 FOREIGN KEY (task_id) REFERENCES task (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F8DB60186');
+        $this->addSql('DROP TABLE message');
+    }
+}
