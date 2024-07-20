93d95c19e85c9e70d7e466404150d8a6a0fe8b5f
Jonathan <jlslew@users.noreply.github.com>
2024-07-20 16:06:10 +0400

    Add symfony debug recipes

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
