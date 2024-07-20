93e708e2250e048285ab268ca6ac0eef9a6a219b
Jonathan <jlslew@users.noreply.github.com>
2024-07-20 13:54:20 +0400

    Update home page with the list of tasks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function index(): Response
+    public function index(TaskRepository $repository): Response
     {
-        return new Response('Hello');
+        return $this->render('home/index.html.twig', [
+            'tasks' => $repository->getAllTasksHomePage(),
+        ]);
     }
 }
